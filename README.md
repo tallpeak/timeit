1ac1c1237b2071dbd6c1d8384757b7c39abfc76b
Aaron W. West <tallpeak@hotmail.com>
2023-04-22 16:01:41 -0700

    this version builds for me in vc2022

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 
 # Win64 command-line timing utility, like Bash's time
 
-# Originally taken from https://github.com/HPCE/hpce-2018-cw1/blob/master/src/mingw64-time.cpp
+Originally taken from https://github.com/HPCE/hpce-2018-cw1/blob/master/src/mingw64-time.cpp
+
+The version formerly in this repo didn't compile for me 
+on a new computer so I've replaced it with one that does
+(Using vs2022 community)
+
--- timeit/timeit.cpp
+++ timeit/timeit.cpp
@@ -1,26 +1,37 @@
+// timeit.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+
 // timeit.cpp 
+//
+//#define _AFXDLL
 
 // stolen from https://github.com/HPCE/hpce-2018-cw1/blob/master/src/mingw64-time.cpp
 // Small changes, to convert from Mingw to Win32
 // converted commandLine to unicode (LPWSTR), to match _tmain
 
-#define WIN32_LEAN_AND_MEAN
+//#define WIN32_LEAN_AND_MEAN
+//
+//#include <atlstr.h>
 
 //#include "stdafx.h"
 #include <windows.h>
+//#include <minwinbase.h>
 
 #include <string>
 #include <math.h>
 
-double toSeconds(const FILETIME& f)
-{
-    double x = f.dwLowDateTime + ldexp(f.dwHighDateTime, 32);
+double toSeconds( const FILETIME &f)
+{ 
+    double x = f.dwLowDateTime + ldexp(f.dwHighDateTime , 32);
     return x * 1e-7;
 }
 
-#include "pch.h"
-#include "framework.h"
-#include "timeit.h"
+//#include "pch.h"
+//#include "framework.h"
+//#include "timeit.h"
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
@@ -28,36 +39,47 @@ double toSeconds(const FILETIME& f)
 
 // The one and only application object
 
-CWinApp theApp;
+//CWinApp theApp;
 
 using namespace std;
 
-int _tmain(int argc, _TCHAR* argv[])
+//https://stackoverflow.com/questions/2573834/c-convert-string-or-char-to-wstring-or-wchar-t
+int StringToWString(std::wstring& ws, const std::string& s)
+{
+    std::wstring wsTmp(s.begin(), s.end());
+
+    ws = wsTmp;
+
+    return 0;
+}
+
+
+int main(int argc, char* argv[])
 {
     int nRetCode = 0;
 
-    HMODULE hModule = ::GetModuleHandle(nullptr);
+    //HMODULE hModule = ::GetModuleHandle(nullptr);
 
-    if (hModule != nullptr)
-    {
-        // initialize MFC and print and error on failure
-        if (!AfxWinInit(hModule, nullptr, ::GetCommandLine(), 0))
-        {
-            // TODO: code your application's behavior here.
-            wprintf(L"Fatal Error: MFC initialization failed\n");
-            nRetCode = 1;
-        }
-        else
-        {
-            // TODO: code your application's behavior here.
-        }
-    }
-    else
-    {
-        // TODO: change error code to suit your needs
-        wprintf(L"Fatal Error: GetModuleHandle failed\n");
-        nRetCode = 1;
-    }
+    //if (hModule != nullptr)
+    //{
+    //    // initialize MFC and print and error on failure
+    //    if (!AfxWinInit(hModule, nullptr, ::GetCommandLine(), 0))
+    //    {
+    //        // TODO: code your application's behavior here.
+    //        wprintf(L"Fatal Error: MFC initialization failed\n");
+    //        nRetCode = 1;
+    //    }
+    //    else
+    //    {
+    //        // TODO: code your application's behavior here.
+    //    }
+    //}
+    //else
+    //{
+    //    // TODO: change error code to suit your needs
+    //    wprintf(L"Fatal Error: GetModuleHandle failed\n");
+    //    nRetCode = 1;
+    //}
 
     /////////////////////////////
 
@@ -66,11 +88,15 @@ int _tmain(int argc, _TCHAR* argv[])
         return 1;
     }
 
-    std::wstring commandLine = argv[1];
+    std::string commandLine;
+    std::wstring wCommandLine;
+    commandLine = argv[1];
     for (int i = 2; i < argc; i++) {
-        commandLine += L" ";
+        commandLine += " ";
         commandLine += argv[i];
     }
+    StringToWString(wCommandLine, commandLine);
+
 
     STARTUPINFO si;
     PROCESS_INFORMATION pi;
@@ -79,9 +105,14 @@ int _tmain(int argc, _TCHAR* argv[])
     si.cb = sizeof(si);
     ZeroMemory(&pi, sizeof(pi));
 
+    // turn that ascii commandline into unicode 
+    /*CStringW cmdLineW(commandLine.c_str());
+    LPCWSTR cmdLineWW = (LPCWSTR)cmdLineW;
+    std::wstring cmdLineWWW(cmdLineWW);*/
+
     BOOL ret = CreateProcess(
         /*lpApplicationName*/ NULL,
-        const_cast<LPWSTR>(commandLine.c_str()),
+        const_cast<LPWSTR> ( wCommandLine.c_str() ) , //const_cast<LPWSTR>(commandLine.c_str()),
         /*lpProcessAttributes*/ NULL,
         /*lpThreadAttributes*/ NULL,
         /*bInheritHandles*/ TRUE,
@@ -129,4 +160,3 @@ int _tmain(int argc, _TCHAR* argv[])
 
     return nRetCode;
 }
-
